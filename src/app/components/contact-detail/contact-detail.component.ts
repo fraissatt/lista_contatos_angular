3bf899619118f209214dc44dc509c4d0d168c004
João Vítor Mamede <jvalencarmamede@gmail.com>
2025-04-14 20:43:45 -0300

    Componente de detalhes implementado

--- src/app/components/contact-detail/contact-detail.component.ts
+++ src/app/components/contact-detail/contact-detail.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ContactService } from '../../services/contact.service';
+import { Contact } from '../../models/contact';
+import { CommonModule } from '@angular/common'; // Necessário para *ngIf
 
 @Component({
   selector: 'app-contact-detail',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './contact-detail.component.html',
-  styleUrl: './contact-detail.component.css'
 })
-export class ContactDetailComponent {
+export class ContactDetailComponent implements OnInit {
+  contato?: Contact;
 
+  constructor(
+    private route: ActivatedRoute,
+    private contactService: ContactService
+  ) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.contato = this.contactService.getContatoById(id);
+  }
 }
